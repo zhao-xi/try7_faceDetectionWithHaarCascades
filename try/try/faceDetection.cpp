0ad512a6c617f99c28828928e88fba1c1486e0d5
DESKTOP-HHBA55H\zhaox <zhaoxi0418@outlook.com>
2018-04-19 22:54:52 +0900

    smile detection

--- try/try/faceDetection.cpp
+++ try/try/faceDetection.cpp
@@ -10,43 +10,53 @@ using namespace std;
 using namespace cv;
 
 string faceCascadePath;
+string smileCascadePath;
 CascadeClassifier faceCascade;
+CascadeClassifier smileCascade;
 
 int main() {
 	faceCascadePath = "haarcascade_frontalface_default.xml";
+	smileCascadePath = "haarcascade_smile.xml";
 	if (!faceCascade.load(faceCascadePath)) {
 		printf("--(!)Error Loading Cascade\n");
 		return -1;
 	}
-	VideoCapture cam(0);
-	if (!cam.isOpened()) { 
-		cout << "fail to open camera\n";
-		return -1;
+	if (!smileCascade.load(smileCascadePath)) {
+		cout << "fail to load smile cascade\n";
 	}
-	int frame_width = cam.get(CAP_PROP_FRAME_WIDTH);
-	int frame_height = cam.get(CAP_PROP_FRAME_HEIGHT);
 	vector<Rect> faces;
+	vector<Rect> smiles;
 
 	while (true) {
-		Mat frame;
-		cam >> frame;
+		Mat frame = imread("hillary_clinton.jpg");
 		Mat frameGray;
 		cvtColor(frame, frameGray, COLOR_BGR2GRAY);
 
 		//	frameClone = frame.clone();
-		faceCascade.detectMultiScale(frameGray, faces, 1.2, 3);
+		faceCascade.detectMultiScale(frameGray, faces, 1.2, 21);
+
 			for (size_t i = 0; i < faces.size(); i++) {
 				int x = faces[i].x;
 				int y = faces[i].y;
 				int w = faces[i].width;
 				int h = faces[i].height;
 				rectangle(frame, Point(x, y), Point(x + w, y + h), Scalar(255, 0, 0), 2, 4);
+				
+				Mat faceROI = frameGray(faces[i]);
+				smileCascade.detectMultiScale(faceROI, smiles, 1.2, 256);
+				for (size_t j = 0; j < smiles.size(); j++) {
+					int smileX = smiles[j].x;
+					int smileY = smiles[j].y;
+					int smileW = smiles[j].width;
+					int smileH = smiles[j].height;
+					rectangle(frame, Point(smileX+x, smileY+y), Point(smileX+x + smileW, smileY+y + smileH), Scalar(0, 255, 0), 2, 4);
+
+				}
 			}
 			//	putText(frameClone, format("# Neighbors = %d", neigh), Point(10, 50), FONT_HERSHEY_SIMPLEX, 1, Scalar(0,0,255), 4);
 		imshow("Face Detection Demo", frame);
 		int k = waitKey(25);
 		if (k == 27) break;
 	}
-		cam.release();
 		destroyAllWindows();
 }
